911141bbe096c7aa22004da8e15fe2e51b8c8c5e
Leuchtkafer <merylins5@yandex.ru>
2016-10-07 20:29:27 +0300

    ÐÐСИсистема сÐбокрки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Выпадающее меню в мобильной версии
+
 var navMain = document.querySelector('.page-header__nav');
 var navToggle = document.querySelector('.page-header__toggle');
 
@@ -12,3 +14,21 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('page-header--opened');
   }
 });
+
+//Модальное окно
+
+var makeOrder = document.querySelector('.make-order');
+var modalWindow = document.querySelector('.modal');
+var modalShadow = document.querySelector('.modal-shadow');
+
+makeOrder.addEventListener('click', function() {
+  event.preventDefault();
+  modalWindow.classList.remove('element-invisible');
+  modalShadow.classList.remove('element-invisible');
+});
+
+modalShadow.addEventListener('click', function() {
+  modalWindow.classList.add('element-invisible');
+  modalShadow.classList.add('element-invisible');
+});
+
