5e672d5d70fdc659a9ef96617bda7bfc016038cb
Leuchtkafer <merylins5@yandex.ru>
2016-10-11 05:32:31 +0300

    SVG 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 ;(function(window, document) {
   'use strict';
-  var file = 'img/symbols.svg',
+  var file = 'build/img/symbols.svg',
     revision = 1;
   if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) return true;
   var isLocalStorage = 'localStorage' in window && window['localStorage'] !== null,
