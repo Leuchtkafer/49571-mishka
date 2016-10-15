0fb246d6f9fba8c00f1031079f645a1e62c218f6
Leuchtkafer <merylins5@yandex.ru>
2016-10-15 14:25:30 +0300

    Check 2

--- js/script.js
+++ js/script.js
@@ -47,11 +47,11 @@ var navToggle = document.querySelector('.page-header__toggle');
 navMain.classList.remove('page-header__nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('page-header--closed')) {
-    navMain.classList.remove('page-header--closed');
+  if (navMain.classList.contains('page-header__nav--closed')) {
+    navMain.classList.remove('page-header__nav--closed');
     navMain.classList.add('page-header--opened');
   } else {
-    navMain.classList.add('page-header--closed');
+    navMain.classList.add('page-header__nav--closed');
     navMain.classList.remove('page-header--opened');
   }
 });
